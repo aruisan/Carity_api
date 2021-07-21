63b60b5c6fd51ce641a3a51ca4f972b40c97beaf
aruisan <oskrjag@gmail.com>
2021-07-21 07:00:27 -0500

    se habilito la carpeta estatic public

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
+app.use(express.static(__dirname + '/uploads'));
 app.use(morgan("dev"));
 app.use(cors());
 app.set("json spaces", 4);
